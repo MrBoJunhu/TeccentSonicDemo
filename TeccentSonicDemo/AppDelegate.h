f9b8ab011b514c44328ea758b55ac405c3902577
BillBo <496081649@qq.com>
2017-08-09 13:46:04 +0800

    初始化工程

---
+++ TeccentSonicDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeccentSonicDemo
+//
+//  Created by BillBo on 2017/8/9.
+//  Copyright © 2017年 BillBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeccentSonicDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeccentSonicDemo
+//
+//  Created by BillBo on 2017/8/9.
+//  Copyright © 2017年 BillBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
